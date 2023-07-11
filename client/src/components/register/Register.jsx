e229be2ec9b8330c33037804b90892d356723359
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-07-11 10:57:07 +0100

    Create the register page

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const onChange = (e) => {
+  //   setUserData((previousValues) => ({
+  //     ...previousValues,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(userData);
+    await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
   return (
-    <form className="register">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button type="button">Register</button>
+    <form className="register" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        // name="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        // name="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button>Register</button>
     </form>
   );
 };
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+  const {username, password} = req.body;
+  res.json({requestData: {username, password}});
+  // res.json('test Ok');
+});
+
+app.listen(4000);

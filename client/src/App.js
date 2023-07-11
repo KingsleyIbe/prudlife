9585ddf1248d3a8c27fab163fe5e5cad1952cd79
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-07-11 02:25:48 +0100

    Add header component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div>coming soon</div>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <img src="../src/assets/images/card-img-7.png" alt="blog" />
+        <h2>How to become a great developer in less than one year</h2>
+        <p>How to become a great developer in less than one year, How to become a great developer in less than one year</p>
+        
+      </div>
+    </main>
+
   );
 }
 

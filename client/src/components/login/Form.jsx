cbfc85c9e5cb14f78ef73e3d6eb6bd8acfc0028c
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-07-11 09:11:19 +0100

    Create the register page

---
+++ client/src/components/login/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button type="button">Login</button>
+    </form>
+  );
+};
+
+export default Form;
--- client/src/components/register/index.js
+++ client/src/components/register/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Form from './Form';
+import Register from './Register';
 
 const index = () => {
   return (
     <div>
-      <Form />
+      <Register />
     </div>
   );
 };

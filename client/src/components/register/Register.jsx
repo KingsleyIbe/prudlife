25f48886ba9827a63f0a678235576e53258b5a6d
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-07-12 11:27:35 +0100

    Hash password

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState({
+    username: '',
+    password: ''
+  });
 
-  // const onChange = (e) => {
-  //   setUserData((previousValues) => ({
-  //     ...previousValues,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
+  const onChange = (e) => {
+    setUserData((previousValues) => ({
+      ...previousValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.log('clicked');
     await fetch('http://localhost:4000/register', {
       method: 'POST',
-      body: JSON.stringify({username, password}),
+      // body: JSON.stringify({username, password}),
+      body: JSON.stringify(userData),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -35,16 +40,18 @@ const Register = () => {
       <input
         type="text"
         placeholder="Username"
-        value={username}
-        // name="username"
-        onChange={(e) => setUsername(e.target.value)}
+        value={userData.username}
+        name="username"
+        // onChange={(e) => setUsername(e.target.value)}
+        onChange={onChange}
       />
       <input
         type="password"
         placeholder="Password"
-        value={password}
-        // name="password"
-        onChange={(e) => setPassword(e.target.value)}
+        value={userData.password}
+        name="password"
+        // onChange={(e) => setPassword(e.target.value)}
+        onChange={onChange}
       />
       <button>Register</button>
     </form>

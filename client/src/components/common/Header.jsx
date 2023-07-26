cbbd2e8027d1e6242c6b9d9fb1fbaecaf6093b3c
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-07-26 14:29:13 +0100

    Implement profile feature

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,15 +1,45 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo?.username);
+      });
+    });
+  }, []);
+
+  const logout = () => {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    })
+  }
+
   return (
     <header>
-        <Link to="/" className="logo">MyBlog</Link>
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </nav>
-      </header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username ? (
+          <>
+            <Link to="/create-new-blog">Create new blog</Link>
+            <button type="button" onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
   );
-}
- 
+};
+
 export default Header;

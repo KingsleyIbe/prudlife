d02d538d1087f89d02754aec5edbeba0320e5333
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-07-30 12:04:40 +0100

    Remove unnecessary comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Layout from "./components/Layout";
-// import Header from './components/common/Header';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
